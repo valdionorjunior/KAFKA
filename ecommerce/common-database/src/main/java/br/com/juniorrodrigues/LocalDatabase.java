4eab7a0ff1b8687602e29560a84a8c452c0b3e4a
Valdionor Junior <valdionorjunior@outlook.com>
2022-04-26 10:21:59 -0300

    [UPDATE] - extraido banco para novo modulo comum

---
+++ ecommerce/common-database/src/main/java/br/com/juniorrodrigues/LocalDatabase.java
@@ -0,0 +1,40 @@
+package br.com.juniorrodrigues;
+
+import java.sql.*;
+
+public class LocalDatabase {
+
+    private final Connection connection;
+    // Assumindo que esse serviço via dor uma unica vez, criarei uma conexão com o um banco de dados pra salvar aas informçeos
+    public LocalDatabase(String name) throws SQLException {
+        // aqui no construtor vamos aabrir a conexao com o banco
+        String url ="jdbc:sqlite:target/"+ name +".db";// criando o arquivo dentro do diretotio target do projeto
+        this.connection = DriverManager.getConnection(url);
+    }
+    // yes this is way to generic.
+    // according to your database tool, avoid injection.
+    public void createIfNotExists(String sql){
+        try {
+            connection.createStatement().execute(sql);
+        }catch (SQLException ex){
+            // be careful, the sql could be wrong, be really careful
+            ex.printStackTrace();
+        }
+    }
+
+    private PreparedStatement prepare(String statement, String[] params) throws SQLException {
+        var prepareStatement = connection.prepareStatement(statement); //o statement pra manipular o banco, no caso um insert
+        for (int i = 0; i < params.length; i++) {
+            prepareStatement.setString(i + 1, params[i]);
+        }
+        return prepareStatement;
+    }
+
+    public boolean update(String statement, String ... params) throws SQLException {
+        return prepare(statement, params).execute();
+    }
+
+    public ResultSet query(String query, String ... params) throws SQLException {
+        return prepare(query, params).executeQuery();
+    }
+}
--- ecommerce/service-users/src/main/java/br/com/juniorrodrigues/CreateUserService.java
+++ ecommerce/service-users/src/main/java/br/com/juniorrodrigues/CreateUserService.java
@@ -1,32 +1,21 @@
 package br.com.juniorrodrigues;
 
 import br.com.juniorrodrigues.consumer.ConsumerService;
-import br.com.juniorrodrigues.consumer.KafkaService;
 import br.com.juniorrodrigues.consumer.ServiceRunner;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 public class CreateUserService implements ConsumerService<Order> {
-    private final Connection connection;
-    // Assumindo que esse serviço via dor uma unica vez, criarei uma conexão com o um banco de dados pra salvar aas informçeos
-    private CreateUserService() throws SQLException {
-        // aqui no construtor vamos aabrir a conexao com o banco
-        String url ="jdbc:sqlite:target/users_database.db";// criando o arquivo dentro do diretotio target do projeto
-        this.connection = DriverManager.getConnection(url);
-        try {
-            connection.createStatement().execute("create table Users (" +
-                    "uuid varchar(200) primary key," +
-                    "email varchar(200))");
-        }catch (SQLException ex){
-            // be careful, the sql could be wrong, be really careful
-            ex.printStackTrace();
-        }
+
+    private final LocalDatabase database;
+
+    public CreateUserService() throws SQLException {
+        this.database = new LocalDatabase("users_database");
+        this.database.createIfNotExists("create table Users (" +
+                "uuid varchar(200) primary key," +
+                "email varchar(200))");
     }
 
     @Override
@@ -59,20 +48,14 @@ public class CreateUserService implements ConsumerService<Order> {
     }
 
     private void insertNewUser( String email) throws SQLException {
-        var insert = connection.prepareStatement("insert into Users (uuid, email) values (?,?)"); //o statement pra manipular o banco, no caso um insert
         var uuid = UUID.randomUUID().toString();
-        insert.setString(1, uuid);//gera novo uuid
-        insert.setString(2, email);
-        insert.execute();
+        database.update("insert into Users (uuid, email) values (?,?)", uuid, email);
         System.out.println("Usuario "+ uuid +" e "+email+" adicionado.");
     }
 
     private boolean isNewUser(String email) throws SQLException {
-        var exists = connection.prepareStatement("select uuid from Users " +
-                "where email = ? limit 1");// traz o usuario pelo uuid onde o email é igual ao que vir, onde sempre trara somente 1 registro
-        exists.setString(1, email);
-        var result = exists.executeQuery();
-
+        var result = database.query("select uuid from Users " +
+                "where email = ? limit 1", email);
         return !result.next(); // se vai pra procima linha é ppq existe usuario
 
     }
